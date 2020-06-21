4013dd388c71ea36a39900a91cc6087d5b6c4a91
Fabian Rang <fabian.rang@yahoo.de>
2020-06-21 13:20:12 +0200

    added: Added starter code including update for microservices

---
+++ udagram-api-feed/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=Kakamaus123
+export POSTGRES_PASSWORD=Kakamaus123
+export POSTGRES_HOST=udagramdb.ctgqolrd04bw.eu-central-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udagram-7773-6228-9718-dev
+export AWS_REGION=eu-central-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
